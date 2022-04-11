e5319dfe1dfd88cd962b397ec754d2b32cd5d8e6
Anupam mathur <45711832+anupam9546@users.noreply.github.com>
2022-04-11 12:17:37 +0530

    Create ar.java

---
+++ Day-1(11-04-2022)/ar.java
@@ -0,0 +1,31 @@
+package array1;
+import java.util.Scanner;
+
+public class ar {
+	
+	public static void main(String[] args)   
+	{  
+	Scanner sc=new Scanner(System.in);  
+	System.out.print("Start entering the elements : ");
+	String str="";
+	for(int i=0;;i++)
+	{
+		int a=sc.nextInt();
+		if (a!=-1)
+		{
+			String s=Integer.toString(a);
+			str=str+s+" ";
+		}
+		else
+			break;
+	}
+	String[] s= str.split(" ");
+	System.out.print("\nArray is : [");
+    for (int i = 0; i < s.length; i++) {
+        System.out.print(s[i] + " ");
+    }
+    System.out.print("]");
+	System.out.println("\nThe Size of the array is :"+s.length);
+	}
+}
+
