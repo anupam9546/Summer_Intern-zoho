2e9ae27eed534bb67a9e5faf0b33e8db2d287363
Anupam mathur <45711832+anupam9546@users.noreply.github.com>
2022-04-12 17:53:40 +0530

    Create itemset.java

---
+++ Day-2(12-04-2022)/Supermarket/itemset.java
@@ -0,0 +1,13 @@
+package com.oops.day2;
+
+class itemset {
+
+	 //make a 2d arraylist to store the items and discounts associated with that
+	void itemset(String item,int rate,String free,int Subs_dis,int item_dis,int cat_dis) 
+	/*to store all the items available in the store with the prices and discount*/
+	{
+		
+		 
+	}
+	
+}
